2753105755fdd53448e1a526888174aee939071e
goranstevanovic <stevanovicgoran@proton.me>
2026-02-17 13:18:51 +0100

    Create authReducer

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -6,3 +6,50 @@ const initialState: AuthState = {
   error: undefined,
   isLoading: false,
 };
+
+function authReducer(state: AuthState, action: AuthAction): AuthState {
+  switch (action.type) {
+    // Same state returned for both LOGIN and SIGNUP actions
+    case "LOGIN":
+    case "SIGNUP": {
+      return {
+        user: action.payload,
+        isAuthenticated: true,
+        error: undefined,
+        isLoading: false,
+      };
+    }
+
+    // Reset state to the default/initial state
+    case "LOGOUT": {
+      return initialState;
+    }
+
+    case "SET_ERROR": {
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false,
+      };
+    }
+
+    case "CLEAR_ERROR": {
+      return {
+        ...state,
+        error: undefined,
+      };
+    }
+
+    case "SET_LOADING": {
+      return {
+        ...state,
+        isLoading: action.payload,
+      };
+    }
+
+    // No matching actions
+    default: {
+      return state;
+    }
+  }
+}
