e47355f978f6cfaf8056b9824aadb70032894c18
goranstevanovic <stevanovicgoran@proton.me>
2026-02-24 20:03:40 +0100

    Configure ESLint and Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
-const { defineConfig } = require("eslint/config");
-const expoConfig = require("eslint-config-expo/flat");
-const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const prettierConfig = require('eslint-config-prettier');
 
 module.exports = defineConfig([
   expoConfig,
-  eslintPluginPrettierRecommended,
+
+  // Disable ESLint formatting rules
+  prettierConfig,
+
   {
-    ignores: ["dist/*"],
+    ignores: ['dist/*'],
   },
 ]);
