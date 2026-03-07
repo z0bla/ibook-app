fdf1744356425f8e65f8ba9a182c3d2043558179
Goran Stevanović <stevanovicgoran@proton.me>
2026-03-07 18:12:36 +0100

    Use styles object for styles

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -6,6 +6,7 @@ import {
   Text,
   Pressable,
   ActivityIndicator,
+  StyleSheet,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
@@ -13,8 +14,21 @@ import { useAuth } from '@/hooks/useAuth';
 
 export default function LoginScreen() {
   return (
-    <View style={{ padding: 20, flex: 1, justifyContent: 'center' }}>
-      LoginScreen
+    <View style={styles.container}>
+      <Text style={styles.title}>LoginScreen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+});
