f437aa712d64d0574ceca6933ac7c255722b3317
Goran Stevanović <stevanovicgoran@proton.me>
2026-03-14 18:51:23 +0100

    Ignore case in email validation

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -30,7 +30,7 @@ export default function LoginScreen() {
       return false;
     }
 
-    if (!/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/.test(email)) {
+    if (!/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/i.test(email)) {
       setEmailError('Invalid email format');
       return false;
     }
