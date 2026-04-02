596845784f74ec423b36e5d616b9696368fc7af9
neredmaggot <neredmaggot@gmail.com>
2026-04-02 22:43:42 +0200

    Task 2.1/types for services (#59)
    * Create basic screen with placeholder text
    
    * Load LoginScreen at app start by default
    
    * Create main app screen
    
    * Use styles object for styles
    
    * Update title
    
    * Create basic login form with no functionality
    
    * Handle email and password input field change
    
    * Move view when keyboard is displayed
    
    * created useAuth hook
    
    * a
    
    * Handle login, display main app screen on success
    
    * Implement password show/hide button
    
    * Handle email verification
    
    * Handle empty password field
    
    * Show global state error, if available
    
    * Disable login button while logging in
    
    * Show progress indicator after pressing Log in
    
    * Add sign up button
    
    * Fix TypeScript error for App route
    
    * Add button to handle forgotten password
    
    * Clear global error state when editing email and/or password
    
    * Use Pressable instead of Button for password show/hide
    
    * Use react-native-paper Button, Text, TextInput
    
    * Remove ActivityIndicator
    
    Button from react-native-paper has a 'loading' property. If it is true,
    it will display a spinner.
    
    * Change styling
    
    * Show icon in password field for toggling password visibility
    
    * Remove unnecessary Text element inside Button
    
    * Validate email on blur event
    
    * Set mode to text on Forgot password button
    
    * Update email regex for validation
    
    * Use label instead of placeholder on text inputs
    
    * Ignore case in email validation
    
    * Style error messages
    
    * Update package.json
    
    * Show user's name in main app screen after loggin in
    
    * added moduleResolution
    
    * Remove moduleResolution setting
    
    * finally created signup screen with all functionalities working
    
    * Changed confirm password error message and added comments
    
    * Created types for Category, Salon and Service
    
    * added JSDoc comments
    
    * Update salon.types.ts
    
    * Update salon.types.ts
    
    ---------
    
    Co-authored-by: Goran Stevanović <stevanovicgoran@proton.me>
    Co-authored-by: z0bla <rn.blazana@gmail.com>

---
+++ src/types/salon.types.ts
@@ -0,0 +1,33 @@
+import { IconSource } from 'react-native-paper/lib/typescript/components/Icon';
+
+/**Type representing a Category */
+export interface Category {
+  id: string; //unique identifier for a category
+  name: string; //name of the category
+  icon: IconSource; //descriptive icon for a category
+  description: string; //category description
+}
+
+/**Type representing a Salon */
+export interface Salon {
+  id: string; //unique identifier for a salon
+  categoryId: string; //unique identifier for a category
+  name: string; //name of the salon
+  address: string; //address of the salon
+  phone: string; //phone number
+  description: string; //description
+  rating: number; //salon's rating
+  reviewCount: number; //number of reviews
+  image: string; //URL to an image
+  operatingHours: string; //salon's open hours as an array of opening hours for each day of the week
+}
+
+/**Type representing a Service */
+export interface Service {
+  id: string; //unique identifier for the service
+  salonId: string; //unique identifier for a salon
+  name: string; //name or description of the service
+  duration: number; //duration of the service
+  price: number; //price for the service
+}
+
