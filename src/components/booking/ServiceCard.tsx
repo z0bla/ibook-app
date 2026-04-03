91335d6be0e40bf693ba9276bcb17cc5164144e8
Neredmaggot <neredmaggot@gmail.com>
2026-04-04 00:13:26 +0200

    Created the service card component

---
+++ src/components/booking/ServiceCard.tsx
@@ -0,0 +1,23 @@
+import { Service } from '@/types/salon.types';
+import { Button, Card, Text } from 'react-native-paper';
+import { GestureResponderEvent, StyleSheet, View } from 'react-native';
+
+export interface ServiceCardProps {
+  service: Service;
+  onSelectCallback: (e: GestureResponderEvent) => void;
+}
+
+export default function ServiceCard(props: ServiceCardProps) {
+  return (
+    <Card>
+      <Card.Content>
+        <Text variant="bodyLarge">Name: {props.service.name}</Text>
+        <Text variant="bodyMedium">Duration: {props.service.duration} min</Text>
+        <Text variant="bodyMedium">Price: {props.service.price}$</Text>
+      </Card.Content>
+      <Card.Actions>
+        <Button onPress={props.onSelectCallback}>Select</Button>
+      </Card.Actions>
+    </Card>
+  );
+}
