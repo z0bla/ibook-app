7598d0a5e0789e1e56f481630927b955a46abb6d
Goran Stevanović <stevanovicgoran@proton.me>
2026-04-08 12:40:50 +0200

    Create getNext4Weeks() function which returns array of next 4 weeks

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -1,5 +1,16 @@
-import { format } from 'date-fns';
+import { addDays, format, startOfToday } from 'date-fns';
 
 export function formatDate(date: Date): string {
   return format(date, 'EEE, MMM d');
+}
+
+export function getNext4Weeks(): Date[] {
+  const today = startOfToday();
+  const weeks: Date[] = [];
+
+  for (let i = 0; i < 28; i++) {
+    weeks.push(addDays(today, i));
+  }
+
+  return weeks;
 }
