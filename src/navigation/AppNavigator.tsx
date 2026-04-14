d9e1b06a407a8aff1ac92d79c4dd13a25517598c
Neredmaggot <neredmaggot@gmail.com>
2026-04-14 23:19:11 +0200

    created bottom tabs@

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,75 @@
+import { AppTabParamList, BookingStackParamList } from '@/navigation/types';
+import ProfileScreen from '@/screens/auth/ProfileScreen';
+import HomeScreen from '@/screens/home/HomeScreen';
+import CategoriesScreen from '@/screens/categories/CategoriesScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import UpcomingAppointmentsScreen from '@/screens/appointments/UpcomingAppointmentsScreen';
+import { Icon } from 'react-native-paper';
+import { createStackNavigator } from '@react-navigation/stack';
+import SalonsListScreen from '@/screens/categories/SalonsListScreen';
+import SalonDetailScreen from '@/screens/categories/SalonDetailScreen';
+import { useAuth } from '@/hooks';
+
+const Tabs = createBottomTabNavigator<AppTabParamList>();
+
+export default function AppNavigator() {
+  const cont = useAuth();
+  const number = cont.state.user!.name.length;
+
+  function BrowseStack() {
+    const Stack = createStackNavigator<BookingStackParamList>();
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Categories" component={CategoriesScreen} />
+        <Stack.Screen name="SalonsList" component={SalonsListScreen} />
+        <Stack.Screen name="SalonDetail" component={SalonDetailScreen} />
+      </Stack.Navigator>
+    );
+  }
+
+  return (
+    <Tabs.Navigator screenOptions={{ tabBarLabelPosition: 'below-icon' }}>
+      <Tabs.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = focused ? 'home' : 'home-outline';
+            return <Icon source={iconName} color={color} size={size} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="Browse"
+        component={BrowseStack}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = focused ? 'magnify-plus' : 'magnify';
+            return <Icon source={iconName} color={color} size={size} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="Appointments"
+        component={UpcomingAppointmentsScreen}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = focused ? 'calendar' : 'calendar-outline';
+            return <Icon source={iconName} color={color} size={size} />;
+          },
+          tabBarBadge: number, //dummy number as a length from users name
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = focused ? 'account' : 'account-outline';
+            return <Icon source={iconName} color={color} size={size} />;
+          },
+        }}
+      />
+    </Tabs.Navigator>
+  );
+}
---
+++ src/screens/appointments/UpcomingAppointmentsScreen.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native-paper';
+
+export default function UpcomingAppointmentsScreen() {
+  return <Text>Upcoming Appointmnets Screen not ready yet</Text>;
+}
