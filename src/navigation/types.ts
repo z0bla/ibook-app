60a3babc9e7f725bc3a936693f6fbe812197c2cb
Neredmaggot <neredmaggot@gmail.com>
2026-04-14 14:36:46 +0200

    created new types for navigation

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -8,17 +8,35 @@ export type AuthStackParamList = {
 export type RootStackParamList = {
   Auth: undefined;
   App: undefined;
+  Loading: undefined;
+};
+
+export type AppTabParamList = {
+  Home: undefined;
+  Categories: undefined;
+  Appointments: undefined;
   Profile: undefined;
+};
+
+export type BookingStackParamList = {
   Categories: undefined;
   SalonsList: { categoryId: string };
   SalonDetail: { salonId: string };
+  Calendar: undefined;
+  SelectTime: undefined;
+  Confirmation: undefined;
+  Success: undefined;
+};
+
+export type AppointmentStackParamList = {
+  AppointmentDetail: { appointmentId: string };
 };
 
 export type SalonsListScreenProps = StackScreenProps<
-  RootStackParamList,
+  BookingStackParamList,
   'SalonsList'
 >;
 export type SalonDetailScreenProps = StackScreenProps<
-  RootStackParamList,
+  BookingStackParamList,
   'SalonDetail'
 >;
