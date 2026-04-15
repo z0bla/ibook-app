c4cebd8cc2841f14dacfb97e94934312325b8372
neredmaggot <neredmaggot@gmail.com>
2026-04-15 19:01:35 +0200

    types for navigation (#76)
    * created new types for navigation
    
    * fixed new navigation types

--- src/screens/AppNavigator.tsx
+++ src/screens/AppNavigator.tsx
@@ -1,15 +1,16 @@
-import { RootStackParamList } from '@/navigation/types';
+import { BookingStackParamList, AppTabParamList } from '@/navigation/types';
 import { createStackNavigator } from '@react-navigation/stack';
 import ProfileScreen from './auth/ProfileScreen';
 import CategoriesScreen from './categories/CategoriesScreen';
 import SalonsListScreen from './categories/SalonsListScreen';
 
-const Stack = createStackNavigator<RootStackParamList>();
+const Stack = createStackNavigator<BookingStackParamList>();
+const Stack2 = createStackNavigator<AppTabParamList>();
 
 export default function AppNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack2.Screen name="Profile" component={ProfileScreen} />
       <Stack.Screen name="Categories" component={CategoriesScreen} />
       <Stack.Screen name="SalonsList" component={SalonsListScreen} />
     </Stack.Navigator>
